801cceced0f084af76d88bba3217975b816b5ac9
djimcru <djimcru0801@g.educaand.es>
2025-02-05 14:31:45 +0100

    1

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,11 @@
- def fibonacci
+ def potencia(base, exponete):
+    if exponete == 0:
+        return 1
+    if base == 1:
+        return base
+    else:
+        return base * potencia(base, exponete-1)
+
+print(potencia(2, 3))
+
+    
