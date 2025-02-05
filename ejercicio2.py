c217eb2ceac3a7c3f95050f9a48263f75d7093af
djimcru <djimcru0801@g.educaand.es>
2025-02-05 12:59:40 +0100

    ejercicio1 y 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+""" def fibonacci(n):
     a=0
     b=1
     for i in range(0, n - 1):
@@ -9,3 +9,13 @@ def fibonacci(n):
 
 print(fibonacci(6))
 
+ """
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(6))
