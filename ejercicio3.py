93e4b948a5d957d10f536085be674b9b23254acc
djimcru <djimcru0801@g.educaand.es>
2025-02-06 09:05:59 +0100

    terminado

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,11 +1,9 @@
- def potencia(base, exponete):
-    if exponete == 0:
+def potencia(base, exponente):
+    if exponente == 0:
         return 1
     if base == 1:
         return base
     else:
-        return base * potencia(base, exponete-1)
+        return base * potencia(base, exponente-1)
 
 print(potencia(2, 3))
-
-    
